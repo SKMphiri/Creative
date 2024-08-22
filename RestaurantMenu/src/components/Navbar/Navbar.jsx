e5176cd4f0454f6897af9b654bfa9601160fb3a3
Stelso Mphiri <StelsoMphiri@Gmail.com>
2024-08-22 09:47:11 +0200

    Navbar created

---
+++ RestaurantMenu/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+export const Navbar = () => {
+  const [query, setQuery] = useState("")
+  return (
+    <div className='navbar'>
+     <img src="" alt="" className="logo" />
+      <h1 className='logo-name'>Creative.</h1>
+
+      <div className="search-box">
+        <input type="text" placeholder='Pesquise sao prato preferido' size={50} onChange={e => setQuery(e.target.value)} />
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
