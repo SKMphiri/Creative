b212e6a133310fc72a86942cf43d2aa3af467d67
Stelso Mphiri <StelsoMphiri@Gmail.com>
2024-08-26 13:45:08 +0200

    Home page created

--- RestaurantMenu/src/components/Navbar/Navbar.jsx
+++ RestaurantMenu/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
       <h1 className='logo-name'>Creative.</h1>
 
       <div className="search-box">
-        <input type="text" placeholder='Pesquise sao prato preferido' size={50} onChange={e => setQuery(e.target.value)} />
+        <input type="text" placeholder='Pesquise seu prato preferido' size={50} onChange={e => setQuery(e.target.value)} />
       </div>
 
     </div>
